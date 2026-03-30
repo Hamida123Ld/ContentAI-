ed461d42502d5205a1ccfac6acfbb5a99e87f972
Hamida123Ld <abdouladjali25@gmail.com>
2026-03-30 20:34:25 +0100

    Update App.py

--- App.py
+++ App.py
@@ -10,7 +10,7 @@ st.markdown("---")
 # API Key
 api_key = st.secrets["GEMINI_API_KEY"]
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-2.0-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-8b")
 
 # Inputs
 col1, col2 = st.columns(2)
